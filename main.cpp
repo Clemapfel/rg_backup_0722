579c648bf0809b4eaecea140b5ee671246becea1
Clemapfel <mail@clemens-cords.com>
2022-06-27 23:34:09 +0200

    shader compilation working

--- main.cpp
+++ main.cpp
@@ -17,78 +17,12 @@ int main()
     auto window = Window();
     window.create("test", 400, 300);
 
-    //auto shader = Shader();
+    auto shader = Shader();
 
     SDL_Window* gWindow = window.get_native();
     SDL_GLContext gContext = window.get_context();
 
-    //Generate program
-    auto gProgramID = glCreateProgram();
-
-    //Create vertex shader
-    GLuint vertexShader = glCreateShader( GL_VERTEX_SHADER );
-
-    //Get vertex source
-    const GLchar* vertexShaderSource[] =
-    {
-        "#version 140\nin vec2 LVertexPos2D; void main() { gl_Position = vec4( LVertexPos2D.x, LVertexPos2D.y, 0, 1 ); }"
-    };
-
-    //Set vertex source
-    glShaderSource( vertexShader, 1, vertexShaderSource, NULL );
-
-    //Compile vertex source
-    glCompileShader( vertexShader );
-
-    //Check vertex shader for errors
-    GLint vShaderCompiled = GL_FALSE;
-    glGetShaderiv( vertexShader, GL_COMPILE_STATUS, &vShaderCompiled );
-    if( vShaderCompiled != GL_TRUE )
-    {
-        printf( "Unable to compile vertex shader %d!\n", vertexShader );
-    }
-     
-    //Attach vertex shader to program
-    glAttachShader( gProgramID, vertexShader );
-
-
-    //Create fragment shader
-    GLuint fragmentShader = glCreateShader( GL_FRAGMENT_SHADER );
-
-    //Get fragment source
-    const GLchar* fragmentShaderSource[] =
-            {
-                    "#version 140\nout vec4 LFragment; void main() { LFragment = vec4( 1.0, 1.0, 1.0, 1.0 ); }"
-            };
-
-    //Set fragment source
-    glShaderSource( fragmentShader, 1, fragmentShaderSource, NULL );
-
-    //Compile fragment source
-    glCompileShader( fragmentShader );
-
-    //Check fragment shader for errors
-    GLint fShaderCompiled = GL_FALSE;
-    glGetShaderiv( fragmentShader, GL_COMPILE_STATUS, &fShaderCompiled );
-    if( fShaderCompiled != GL_TRUE )
-    {
-        printf( "Unable to compile fragment shader %d!\n", fragmentShader );
-    }
-      
-    //Attach fragment shader to program
-    glAttachShader( gProgramID, fragmentShader );
-
-
-    //Link program
-    glLinkProgram( gProgramID );
-
-    //Check for errors
-    GLint programSuccess = GL_TRUE;
-    glGetProgramiv( gProgramID, GL_LINK_STATUS, &programSuccess );
-    if( programSuccess != GL_TRUE )
-    {
-        printf( "Error linking program %d!\n", gProgramID );
-    }
+    auto gProgramID = shader.get_program_id();
 
     //Get vertex attribute location
     auto gVertexPos2DLocation = glGetAttribLocation(gProgramID, "LVertexPos2D" );
