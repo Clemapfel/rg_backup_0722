c1aae74a8d21d97c151efcc7c50336257feb3747
Clemapfel <mail@clemens-cords.com>
2022-06-27 20:35:50 +0200

    added angle

---
+++ include/angle.hpp
@@ -0,0 +1,38 @@
+// 
+// Copyright 2022 Clemens Cords
+// Created on 6/27/22 by clem (mail@clemens-cords.com)
+//
+
+#pragma once
+
+#include <cmath>
+
+namespace rat
+{
+    struct Angle
+    {
+        public:
+            Angle(float radians);
+
+            float as_degrees() const;
+            float as_radians() const;
+
+            Angle operator+(const Angle&);
+            Angle operator-(const Angle&);
+            Angle operator*(const Angle&);
+            Angle operator/(const Angle&);
+
+            Angle& operator+=(const Angle&);
+            Angle& operator-=(const Angle&);
+            Angle& operator*=(const Angle&);
+            Angle& operator/=(const Angle&);
+
+        private:
+            float _rads;
+    };
+
+    Angle degrees(float);
+    Angle radians(float);
+}
+
+#include <.src/angle.inl>
